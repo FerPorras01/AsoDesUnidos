5c64cc7aac491cd3366906f83c873d8a41ed5e0a
Sebastián Alfaro Delgado <sebasalfaro1205@gmail.com>
2023-04-21 19:58:25 -0600

    Diseño del Agregar Prestamos

--- app/src/main/java/proyecto/AsoDesUnidos/Controladores/Admin/AgregarPrestamoFragment.java
+++ app/src/main/java/proyecto/AsoDesUnidos/Controladores/Admin/AgregarPrestamoFragment.java
@@ -10,48 +10,20 @@ import android.view.ViewGroup;
 
 import proyecto.AsoDesUnidos.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AgregarPrestamoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AgregarPrestamoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
     public AgregarPrestamoFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AgregarPrestamoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static AgregarPrestamoFragment newInstance(String param1, String param2) {
-        AgregarPrestamoFragment fragment = new AgregarPrestamoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        return new AgregarPrestamoFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            // TODO: Rename and change types of parameters
-            String mParam1 = getArguments().getString(ARG_PARAM1);
-            String mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
